c1e1541ce76e1e53ab6b32bac568bc411fd48bea
Nikola Kostov <timscore25@Nikolas-MacBook-Air.local>
2025-03-23 13:28:51 +0200

    final version 2 i think

--- app.py
+++ app.py
@@ -154,4 +154,4 @@ def renderIndex():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
